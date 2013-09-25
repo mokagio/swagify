6da0b2b411e479e343a339b6f606e76bc7f9080a
Gio <giovanni.lodi42@gmail.com>
2013-09-25 23:23:58 +0100

    Added cross, love, and  warning

--- lib/swagify.rb
+++ lib/swagify.rb
@@ -2,15 +2,30 @@
 #
 # Swagify is a tiny tool to add swag to your Ruby scripts: 
 # symbol and colors, right out of the box.
-
+#
 # Let's go!
 
 class Swagify < Object
 
 	# ##Symbols
 	#
-	# tick, returns ✔
+	# `Swagify::cross` returns ✘
+	def self.cross
+		return "\u2718"
+	end
+
+	# `Swagify::love` returns ♥
+	def self.love
+		return "\u2665"
+	end
+
+	# `Swagify::tick` returns ✔
 	def self.tick
 		return "\u2714"
+	end
+
+	# `Swagify::warning` returns ⚠
+	def self.warning
+		return "\u26A0"
 	end
 end
--- spec/swagify_spec.rb
+++ spec/swagify_spec.rb
@@ -1,10 +1,17 @@
 require 'rspec'
 require File.dirname(__FILE__) + '/../lib/swagify'
 
+def it_symbol_spec(name, unicode)
+	it "has a static #{name} method that returns #{unicode}" do
+		Swagify.send(name).should eq unicode
+	end
+end
+
 describe Swagify do
 	context "class methods" do
-		it "has a tick method that returns \u2714" do
-			Swagify::tick.should eq "\u2714"
-		end
+		it_symbol_spec "cross", "\u2718"
+		it_symbol_spec "love", "\u2665"
+		it_symbol_spec "tick", "\u2714"
+		it_symbol_spec "warning", "\u26A0"
 	end
 end
